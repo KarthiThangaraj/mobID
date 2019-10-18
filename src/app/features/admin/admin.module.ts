d5cf73ccfd53a7ec134726b5def20ce271c7b503
karthi <karthi.t@mitosistech.com>
2019-10-18 18:11:31 +0530

    dialog added

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -5,19 +5,23 @@ import { AdminRoutingModule, AdminRoutingComponents } from './admin-routing.modu
 import { NgCircleProgressModule } from 'ng-circle-progress';
 // import {NgbModule, NgbAlertModule, NgbAccordionModule, NgbModalModule, NgbProgressbarModule} from '@ng-bootstrap/ng-bootstrap';
 import {ProgressBarModule} from "angular-progress-bar"
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogOverviewExampleDialog } from './interceptor/interceptor.component';
 
 @NgModule({
-  declarations: [AdminRoutingComponents],
+  declarations: [AdminRoutingComponents, DialogOverviewExampleDialog],
   imports: [
     CommonModule,
     AdminRoutingModule,
     NgCircleProgressModule,
+    MatDialogModule,
     // NgbModule,
     // NgbAlertModule,
     // NgbAccordionModule,
     // NgbModalModule,
     // NgbProgressbarModule,
     ProgressBarModule
-  ]
+  ],
+  entryComponents: [DialogOverviewExampleDialog]
 })
 export class AdminModule { }
--- src/app/features/admin/interceptor/interceptor.component.ts
+++ src/app/features/admin/interceptor/interceptor.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-interceptor',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterceptorComponent implements OnInit {
 
-  constructor() { }
+  animal: string;
+  name: string;
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250vh',
+      data: {name: this.name, animal: this.animal},
+      panelClass: 'custom-modalbox'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+  styleUrls: ['./interceptor.component.scss']
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
